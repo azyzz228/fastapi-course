4d6caedf469f032025745ff3cfc5979ab3520512
Aziz Abdullaev <aziz.saidmuratov@duke.edu>
2022-03-25 22:17:05 -0400

    connect db to backend
    now we can send POST request and it will automatically write the data to the database

--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,22 @@
-import imp
-from fastapi import FastAPI
+from fileinput import close
+from fastapi import FastAPI, Depends
 from . import schemas, models, database
-
+from sqlalchemy.orm import Session
 app = FastAPI()
 
 models.Base.metadata.create_all(database.engine)
 
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.post("/blog")
-def create(request: schemas.Blog):
-    return {"data": f"passed title {request.title} with body--{request.body}"}
+def create(request: schemas.Blog, db: Session=Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
